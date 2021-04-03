51313125fb1bbe1440a4f859844c02ceb5936c5a
ernestognw <ernestognw@gmail.com>
2021-04-02 20:43:42 -0600

    Step 4: Web app setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+    ],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
